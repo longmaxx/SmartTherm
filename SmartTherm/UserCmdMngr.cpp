96b51bcb0b27676424adec40a6a88623b53e9283
longmaxx <longmaxx@yandex.ru>
2016-01-10 22:39:30 +0300

    обработка команды 1
    не проверялось

--- SmartTherm/UserCmdMngr.cpp
+++ SmartTherm/UserCmdMngr.cpp
@@ -1,6 +1,6 @@
 #include "UserCmdMngr.h"
 
-UserCmdMngr::UserCmdMngr()
+UserCmdMngr::UserCmdMngr() 
 {
   
 }
@@ -13,40 +13,69 @@ void UserCmdMngr::Init(SoftwareSerial* pSWSP)
 
 void UserCmdMngr::SerialPortLoop()
 {
-  boolean bExit = false;
-  this->bufIndex = 0;
-  if (this->SPort->find("CMD:")){// если нашли признак начала команды
-    while(!bExit){
-      char curByte;
-      if (this->SPort->available()>0){
-        curByte = this->SPort->read();
-        if ((curByte == '\r')||(this->bufIndex == (bufLen-1))){
-          this->buf[this->bufIndex]='\0';
-          break;
-        }else{
-          this->buf[this->bufIndex]=curByte;
-        }
-      }
+  boolean bEndFound = false;
+  //this->bufIndex = 0;
+  //считываем  данные из порта пока они есть или до заполнения буфера
+  while (this->SPort->available()>0){
+    char curByte;
+    curByte = this->SPort->read();
+    if (curByte == '\r'){
+      this->buf[this->bufIndex++]='\0';
+      bEndFound = true;
+      break;
+    }else{
+      this->buf[this->bufIndex++]=curByte;
     }
-    //закончили прием слова команды. обрабатываем
+  }
+  // проверяем буфер на наличие команды
+  if ((bEndFound)&&
+      (this->buf[0] == 'C')&&
+      (this->buf[1] == 'M')&&
+      (this->buf[2] == 'D')&&
+      (this->buf[3] == ':')
+                            ){
     unsigned char cmd = this->parseCmdName();
     if (cmd == 1){
       this->Cmd_Hello();
     }
-    
   }
 }
 
 unsigned char UserCmdMngr::parseCmdName()
 {
-  unsigned char foundState[commandsCount];
-  for (unsigned char i=0;i<this->bufIndex;i++){
+  #define ST_NOTFOUND (0)
+  #define ST_OVERFLOW (-2)
+ 
+  #define CMD_BUF_BASE (4)
+  
+  signed char foundState[commandsCount];
+  for (unsigned char k = 0;k<commandsCount;k++ ){
+    foundState[k]=0;// init values
+  }
+  for (unsigned char i=CMD_BUF_BASE;i<this->bufIndex;i++){
     //TODO
     // пробегаем по массиву команд и смотрим не совпадает ли байт с текущим
     for (unsigned char c = 0;c<commandsCount;c++){
-  
+     //const char* cmd_name= this->commands[c];
+     
+      if (foundState[c] != ST_OVERFLOW){
+        if (this->commands[c][i-CMD_BUF_BASE] == this->buf[i]){
+          //байт совпал
+          foundState[c]++;
+        //}else if (cmd_name[c][i-CMD_BUF_BASE] == '\0'){
+        //  foundState[c] = ST_OVERFLOW;
+        }else {
+          foundState[c] = ST_OVERFLOW;
+        }
+      }  
     }
   }
+  // считаем что получилось по поиску
+  for (unsigned char k = 0;k<commandsCount;k++ ){
+    if (foundState[k]!=ST_OVERFLOW)
+      return k;
+  }
+  return 0;
 }
 
 void UserCmdMngr::Cmd_Hello()
--- SmartTherm/UserCmdMngr.h
+++ SmartTherm/UserCmdMngr.h
@@ -3,18 +3,18 @@
 #ifndef USERCMDMNGR_H
   #define USERCMDMNGR_H
 
-  #define bufLen (20)
-  #define commandsCount (1)
+  #define bufLen (32)
+  #define commandsCount (2)
 class UserCmdMngr{
   private: SoftwareSerial* SPort;
   private: char buf[bufLen];// буфер для сохранения данных команды из порта
   private: unsigned char bufIndex;
-  private: const char commands[commandsCount] = {"Hello"};
+  private: const char* commands[commandsCount] = {"hello","settime"};
 
   public: UserCmdMngr();
   public: void Init(SoftwareSerial* pSWSP);
   public: void SerialPortLoop();
-  private: unsigned char UserCmdMngr::parseCmdName();// возвращает индекс команды или 0
+  private: unsigned char parseCmdName();// возвращает индекс команды или 0
   private: void Cmd_Hello();
 };
 #endif  
